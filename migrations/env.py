f26d13a7dd5f3491539469716de73065ca7c5bdd
Alex Momi <alexmomijunior@gmail.com>
2022-11-03 14:41:41 -0300

    Create table task

--- migrations/env.py
+++ migrations/env.py
@@ -9,7 +9,7 @@ from src.config import DB_CONNECTION_URL
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlachemy.url", DB_CONNECTION_URL)
+config.set_main_option("sqlalchemy.url", DB_CONNECTION_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
